3a4e73f1bc0508978015fb1bab0d771c30337661
Rido Sandi Atmanto <ridoatmanto@gmail.com>
2022-11-05 15:51:56 +0700

    Update credit

--- UCL_last_16_draw/UCLlast16Draw.rb
+++ UCL_last_16_draw/UCLlast16Draw.rb
@@ -1,156 +1,159 @@
+# Created by Rido Atmanto (https://github.com/ridoatmanto/ucl-last-16-draw)"
+# November 5th 2022
+
 class UCLlast16Draw
-	def initialize(participants)
-		@group_winners = participants.select{ |x| x[:status] == "group_winner" }
-		@runner_up 	   = participants.select{ |x| x[:status] == "runner_up" }
-   	end
+    def initialize(participants)
+        @group_winners = participants.select{ |x| x[:status] == "group_winner" }
+        @runner_up     = participants.select{ |x| x[:status] == "runner_up" }
+    end
 
-	def opponent_candidate(participants, candidate)
-		participants.select{ |x| x[:country] != candidate[:country] && x[:group] != candidate[:group]}
-	end
+    def opponent_candidate(participants, candidate)
+        participants.select{ |x| x[:country] != candidate[:country] && x[:group] != candidate[:group]}
+    end
 
-	def header(title, area_width)
-		header = "\n"
-		header += "\t"
-		header += title.center(area_width)
-		header += "\n\t" + ("_" * area_width).center(area_width) + "\n\n"
-		
-		header
-	end
+    def header(title, area_width)
+        header = "\n"
+        header += "\t"
+        header += title.center(area_width)
+        header += "\n\t" + ("_" * area_width).center(area_width) + "\n\n"
+        
+        header
+    end
 
-	def generate_draw
-		winners = @group_winners.clone
-		draw = []
+    def generate_draw
+        winners = @group_winners.clone
+        draw = []
 
-		while draw.empty?
-			available_runner_up = @runner_up.clone
+        while draw.empty?
+            available_runner_up = @runner_up.clone
 
-			winners.shuffle.each do |item|
-				candidate = opponent_candidate(available_runner_up, item).sample
-				
-				if candidate.nil?
-					draw = []
-					break
-				end
+            winners.shuffle.each do |item|
+                candidate = opponent_candidate(available_runner_up, item).sample
+                
+                if candidate.nil?
+                    draw = []
+                    break
+                end
 
-				draw.push([item, candidate])
-				available_runner_up.delete(candidate)
-			end
-		end
+                draw.push([item, candidate])
+                available_runner_up.delete(candidate)
+            end
+        end
 
-		generate_draw_by_value(draw, 1)
-	end
+        generate_draw_by_value(draw, 1)
+    end
 
-	def generate_draw_by_value(draw, index)
-		text = header("UEFA Champions League Sample Draw (last 16 Round) ##{index.to_s}", 80)
-		draw.each do |x|
-			item 	  = x.first
-			candidate = x.last
+    def generate_draw_by_value(draw, index)
+        text = header("UEFA Champions League Sample Draw (last 16 Round) ##{index.to_s}", 80)
+        draw.each do |x|
+            item      = x.first
+            candidate = x.last
 
-			max_position = 35
-			winner = "#{item[:name]} (#{item[:country]})/Group #{item[:group]}"
-			
-			text += "\t".ljust(max_position-winner.length)
-			text += "#{item[:name]} (#{item[:country]})/Group #{item[:group]}"
-			text += "  vs  "
-			text += "#{candidate[:name]} (#{candidate[:country]})/Group #{candidate[:group]}\n\n"
-		end
+            max_position = 35
+            winner = "#{item[:name]} (#{item[:country]})/Group #{item[:group]}"
+            
+            text += "\t".ljust(max_position-winner.length)
+            text += "#{item[:name]} (#{item[:country]})/Group #{item[:group]}"
+            text += "  vs  "
+            text += "#{candidate[:name]} (#{candidate[:country]})/Group #{candidate[:group]}\n\n"
+        end
 
-		text + "\n"
-	end
+        text + "\n"
+    end
 
-	def opponent_possibilities
-		text = header("UEFA Champions League Possible Opponent each team", 55)
+    def opponent_possibilities
+        text = header("UEFA Champions League Possible Opponent each team", 55)
 
-		@group_winners.shuffle.each do |item|
-			beginning = 1
-			prefiks = "\t#{item[:name]} (#{item[:country]}) VS "
-			
-			opponent_candidate(@runner_up, item).map do |x|
-				temp = ""
-				if (beginning == 1)
-					temp += prefiks
-					beginning = 0
-				else
-					temp += "\t" + (" " * (prefiks.length-1))
-				end
+        @group_winners.shuffle.each do |item|
+            beginning = 1
+            prefiks = "\t#{item[:name]} (#{item[:country]}) VS "
+            
+            opponent_candidate(@runner_up, item).map do |x|
+                temp = ""
+                if (beginning == 1)
+                    temp += prefiks
+                    beginning = 0
+                else
+                    temp += "\t" + (" " * (prefiks.length-1))
+                end
 
-				temp += "#{x[:name]} (#{x[:country]})\n"
-				text += temp
-			end
+                temp += "#{x[:name]} (#{x[:country]})\n"
+                text += temp
+            end
 
-			text += "\n\n"
-		end
+            text += "\n\n"
+        end
 
-		text
-	end
+        text
+    end
 
-	def all_possibilities
-		text 		  = "\n"
-		number 		  = 1
-		winners       = @group_winners.clone
-		runner_up     = @runner_up.clone
-		possibilities = []
+    def all_possibilities
+        text          = "\n"
+        number        = 1
+        winners       = @group_winners.clone
+        runner_up     = @runner_up.clone
+        possibilities = []
 
-		# Collecting all possibilities
-		winners.length.times do
-			reserved_candidate_by_club = {}
+        # Collecting all possibilities
+        winners.length.times do
+            reserved_candidate_by_club = {}
 
-			winners.each do |item|
-				candidate = opponent_candidate(runner_up, item)
-				reserved_candidate_by_club[item] = candidate
-			end
+            winners.each do |item|
+                candidate = opponent_candidate(runner_up, item)
+                reserved_candidate_by_club[item] = candidate
+            end
 
-			possibilities.push(reserved_candidate_by_club)
-			winners = winners.rotate(1)
-		end
+            possibilities.push(reserved_candidate_by_club)
+            winners = winners.rotate(1)
+        end
 
-		all_combinations = []
-		total 			 = 0
-		all   			 = []
+        all_combinations = []
+        total            = 0
+        all              = []
 
-		# Create General Draw by available candidate
-		possibilities.each_with_index do |value, index|
-			reserved = []
+        # Create General Draw by available candidate
+        possibilities.each_with_index do |value, index|
+            reserved = []
 
-			value.each do |k, v|
-				temp = []
-				v.each do |item|
-					temp.push([k,item])
-				end
-				reserved.push(temp)
-			end
+            value.each do |k, v|
+                temp = []
+                v.each do |item|
+                    temp.push([k,item])
+                end
+                reserved.push(temp)
+            end
 
-			get_all_combination(*reserved).each do |item|
-				if check_valid_draw(item)
-					all_combinations.push(item)
-					total = total + 1 
-				end
-			end
-		end
+            get_all_combination(*reserved).each do |item|
+                if check_valid_draw(item)
+                    all_combinations.push(item)
+                    total = total + 1 
+                end
+            end
+        end
 
-		{total: format_number(total), last_draw: all_combinations[total-1]}
-	end
+        {total: format_number(total), last_draw: all_combinations[total-1]}
+    end
 
-	def get_all_combination(*arr)
-	  arr.shift.product(*arr)
-	end
+    def get_all_combination(*arr)
+      arr.shift.product(*arr)
+    end
 
-	def check_valid_draw(arr)
-		reserved_opponent = []
+    def check_valid_draw(arr)
+        reserved_opponent = []
 
-		arr.each_with_index do |item, index|
-			if reserved_opponent.include?(item.last)
-				return false
-			else
-				reserved_opponent.push(item.last)	
-			end
-		end
+        arr.each_with_index do |item, index|
+            if reserved_opponent.include?(item.last)
+                return false
+            else
+                reserved_opponent.push(item.last)   
+            end
+        end
 
-		true
-	end
+        true
+    end
 
-	def format_number(number)
-	  num_groups = number.to_s.chars.to_a.reverse.each_slice(3)
-	  num_groups.map(&:join).join(',').reverse
-	end
+    def format_number(number)
+      num_groups = number.to_s.chars.to_a.reverse.each_slice(3)
+      num_groups.map(&:join).join(',').reverse
+    end
 end
--- UCL_last_16_draw/example.rb
+++ UCL_last_16_draw/example.rb
@@ -1,29 +1,29 @@
 require './UCLlast16Draw.rb'
 
 participants = [
-	{status: "group_winner", group: "A", name: "Napoli", country: "ITA"},
-	{status: "runner_up", group: "A", name: "Liverpool", country: "ENG"},
-	
-	{status: "group_winner", group: "B", name: "Porto", country: "POR"},
-	{status: "runner_up", group: "B", name: "Club Brugge", country: "BEL"},
+    {status: "group_winner", group: "A", name: "Napoli", country: "ITA"},
+    {status: "runner_up", group: "A", name: "Liverpool", country: "ENG"},
+    
+    {status: "group_winner", group: "B", name: "Porto", country: "POR"},
+    {status: "runner_up", group: "B", name: "Club Brugge", country: "BEL"},
 
-	{status: "group_winner", group: "C", name: "Bayern Munich", country: "GER"},
-	{status: "runner_up", group: "C", name: "Inter Milan", country: "ITA"},
+    {status: "group_winner", group: "C", name: "Bayern Munich", country: "GER"},
+    {status: "runner_up", group: "C", name: "Inter Milan", country: "ITA"},
 
-	{status: "group_winner", group: "D", name: "Tottenham Hotspur", country: "ENG"},
-	{status: "runner_up", group: "D", name: "Eintracht Frankfurt", country: "GER"},
+    {status: "group_winner", group: "D", name: "Tottenham Hotspur", country: "ENG"},
+    {status: "runner_up", group: "D", name: "Eintracht Frankfurt", country: "GER"},
 
-	{status: "group_winner", group: "E", name: "Chelsea", country: "ENG"},
-	{status: "runner_up", group: "E", name: "AC Milan", country: "ITA"},
+    {status: "group_winner", group: "E", name: "Chelsea", country: "ENG"},
+    {status: "runner_up", group: "E", name: "AC Milan", country: "ITA"},
 
-	{status: "group_winner", group: "F", name: "Real Madrid", country: "ESP"},
-	{status: "runner_up", group: "F", name: "RB Leipzig", country: "GER"},
+    {status: "group_winner", group: "F", name: "Real Madrid", country: "ESP"},
+    {status: "runner_up", group: "F", name: "RB Leipzig", country: "GER"},
 
-	{status: "group_winner", group: "G", name: "Manchester City", country: "ENG"},
-	{status: "runner_up", group: "G", name: "Borussia Dortmund", country: "GER"},
+    {status: "group_winner", group: "G", name: "Manchester City", country: "ENG"},
+    {status: "runner_up", group: "G", name: "Borussia Dortmund", country: "GER"},
 
-	{status: "group_winner", group: "H", name: "Benfica", country: "POR"},
-	{status: "runner_up", group: "H", name: "Paris Saint-Germain", country: "FRA"}
+    {status: "group_winner", group: "H", name: "Benfica", country: "POR"},
+    {status: "runner_up", group: "H", name: "Paris Saint-Germain", country: "FRA"}
 ]
 
 result = UCLlast16Draw.new(participants)
@@ -37,4 +37,5 @@ puts "\tTotal Possibilities : #{all[:total]}"
 
 puts result.generate_draw_by_value(all[:last_draw], all[:total])
 
-puts "Created by Rido Atmanto (https://github.com/ridoatmanto/ucl-last-16-draw)"
+puts "\tCreated by Rido Atmanto (https://github.com/RidoAtmanto)"
+puts
