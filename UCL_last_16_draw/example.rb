09206e76e3d19b12b707301dad4ed64130d2ddeb
Rido Sandi Atmanto <ridoatmanto@gmail.com>
2022-11-06 21:46:04 +0700

    Following ruby indentation

--- UCL_last_16_draw/example.rb
+++ UCL_last_16_draw/example.rb
@@ -1,29 +1,29 @@
 require './UCLlast16Draw.rb'
 
 participants = [
-	{status: "group_winner", group: "A", name: "Napoli", country: "ITA"},
-	{status: "runner_up", group: "A", name: "Liverpool", country: "ENG"},
-	
-	{status: "group_winner", group: "B", name: "Porto", country: "POR"},
-	{status: "runner_up", group: "B", name: "Club Brugge", country: "BEL"},
+  {status: "group_winner", group: "A", name: "Napoli", country: "ITA"},
+  {status: "runner_up", group: "A", name: "Liverpool", country: "ENG"},
+  
+  {status: "group_winner", group: "B", name: "Porto", country: "POR"},
+  {status: "runner_up", group: "B", name: "Club Brugge", country: "BEL"},
 
-	{status: "group_winner", group: "C", name: "Bayern Munich", country: "GER"},
-	{status: "runner_up", group: "C", name: "Inter Milan", country: "ITA"},
+  {status: "group_winner", group: "C", name: "Bayern Munich", country: "GER"},
+  {status: "runner_up", group: "C", name: "Inter Milan", country: "ITA"},
 
-	{status: "group_winner", group: "D", name: "Tottenham Hotspur", country: "ENG"},
-	{status: "runner_up", group: "D", name: "Eintracht Frankfurt", country: "GER"},
+  {status: "group_winner", group: "D", name: "Tottenham Hotspur", country: "ENG"},
+  {status: "runner_up", group: "D", name: "Eintracht Frankfurt", country: "GER"},
 
-	{status: "group_winner", group: "E", name: "Chelsea", country: "ENG"},
-	{status: "runner_up", group: "E", name: "AC Milan", country: "ITA"},
+  {status: "group_winner", group: "E", name: "Chelsea", country: "ENG"},
+  {status: "runner_up", group: "E", name: "AC Milan", country: "ITA"},
 
-	{status: "group_winner", group: "F", name: "Real Madrid", country: "ESP"},
-	{status: "runner_up", group: "F", name: "RB Leipzig", country: "GER"},
+  {status: "group_winner", group: "F", name: "Real Madrid", country: "ESP"},
+  {status: "runner_up", group: "F", name: "RB Leipzig", country: "GER"},
 
-	{status: "group_winner", group: "G", name: "Manchester City", country: "ENG"},
-	{status: "runner_up", group: "G", name: "Borussia Dortmund", country: "GER"},
+  {status: "group_winner", group: "G", name: "Manchester City", country: "ENG"},
+  {status: "runner_up", group: "G", name: "Borussia Dortmund", country: "GER"},
 
-	{status: "group_winner", group: "H", name: "Benfica", country: "POR"},
-	{status: "runner_up", group: "H", name: "Paris Saint-Germain", country: "FRA"}
+  {status: "group_winner", group: "H", name: "Benfica", country: "POR"},
+  {status: "runner_up", group: "H", name: "Paris Saint-Germain", country: "FRA"}
 ]
 
 result = UCLlast16Draw.new(participants)
